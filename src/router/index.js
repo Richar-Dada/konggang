380fb11a8687ce4b39135e3b4455ae4c0265b121
Richar-Dada <631151149@qq.com>
2018-03-13 08:44:10 +0800

    Push  添加路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,28 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/views/Index'
 import Home from '@/views/Home'
-import MyOrder from '@/views/MyOrder'
-import Individual from '@/views/Individual'
-import Booking from '@/views/Booking'
-import Schedul from '@/views/Schedul'
-import Remark from '@/views/Remark'
+// import MyOrder from '@/views/MyOrder'
+// import Individual from '@/views/Individual'
+// import Booking from '@/views/Booking'
+// import Schedul from '@/views/Schedul'
+// import Remark from '@/views/Remark'
+/* eslint-disable */
+const MyOrder = () => import('@/views/MyOrder')
+const Individual = () => import('@/views/Individual')
+const Booking = () => import('@/views/Booking')
+const Schedul = () => import('@/views/Schedul')
+const Remark = () => import('@/views/Remark')
 
 // 登录注册
-import Login from '@/views/Login'
-import Registe from '@/views/Registe'
-import CertificateConfirm from '@/views/CertificateConfirm'
-import ResetPassword from '@/views/ResetPassword'
+// import Login from '@/views/Login'
+// import Registe from '@/views/Registe'
+// import CertificateConfirm from '@/views/CertificateConfirm'
+// import ResetPassword from '@/views/ResetPassword'
+/* eslint-disable */
+const Login = () => import('@/views/Login')
+const Registe = () => import('@/views/Registe')
+const CertificateConfirm = () => import('@/views/CertificateConfirm')
+const ResetPassword = () => import('@/views/ResetPassword')
 
 Vue.use(Router)
 
