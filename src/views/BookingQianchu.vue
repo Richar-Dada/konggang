ee5010803782562d5556f96d84fa7fdaca5acccb
Richar-Dada <631151149@qq.com>
2018-03-31 00:46:02 +0800

    Push  迁入预约改造

--- src/views/BookingQianchu.vue
+++ src/views/BookingQianchu.vue
@@ -6,9 +6,9 @@
       <x-input title="发动机号" ref="engineNumber" label-width="2.5rem" required v-model="engineNumber" keyboard="number" :min="4" :max="4" placeholder="必填,发动机号码后四位"></x-input>
       <x-input title="原车主姓名" ref="oldCarOwner" label-width="2.5rem" required v-model="oldCarOwner" placeholder="必填"></x-input>
       <x-input title="新车主姓名" ref="newCarOwner" label-width="2.5rem" required v-model="newCarOwner" placeholder="必填"></x-input>
-      <x-address title="迁入地" v-model="immigrationAddress" raw-value :list="addressData"></x-address>
-      <x-input title="新车主身份证明" ref="carId" label-width="2.5rem" required v-model="carId" placeholder="必填,这输入车牌号"></x-input>
-      <x-input title="新车主证件号码" ref="carId" label-width="2.5rem" required v-model="carId" placeholder="必填,这输入车牌号"></x-input>
+      <x-address class="x-address" title="迁入地" v-model="immigrationAddress" raw-value :list="addressData"></x-address>
+      <selector ref="newCarDocumentType" title="新车主证件" placeholder="请选择一个" :options="documentType" v-model="newCarDocumentType"></selector>
+      <x-input title="新车主证件号码" ref="newCarDocumentNumber" label-width="3.5rem" required v-model="newCarDocumentNumber" placeholder="必填"></x-input>
     </group>
     <div class="function-box">
       <x-button class="submit-btn" type="primary" @click.native="booking">提 交</x-button>
@@ -18,7 +18,7 @@
 </template>
 
 <script>
-import { Group, XInput, Checker, CheckerItem, XTextarea, XButton, Toast, ChinaAddressV4Data, XAddress } from 'vux'
+import { Group, XInput, Checker, CheckerItem, XTextarea, XButton, Toast, ChinaAddressV4Data, Value2nameFilter as value2name, XAddress, Selector } from 'vux'
 import { checkPhone } from '@/utils/validateTool'
 import { booking } from '@/service'
 
@@ -32,7 +32,8 @@ export default {
     XTextarea,
     XButton,
     Toast,
-    XAddress
+    XAddress,
+    Selector
   },
   props: {
     selectedDate: {
@@ -47,15 +48,20 @@ export default {
       engineNumber: '',
       carId: '粤A',
       carname: '',
-      remark: '',
       toastMsg: '',
       bookingDate: '',
       bookingTime: '',
       oldCarOwner: '',
       newCarOwner: '',
-      immigrationAddress: '',
+      immigrationAddress: ['广东省', '广州市', '花都区'],
+      documentType: [
+        { key: '身份证', value: '身份证' },
+        { key: '居住证', value: '居住证' },
+        { key: '统一社会代码', value: '统一社会代码' }],
       addressData: ChinaAddressV4Data,
-      showToast: false
+      showToast: false,
+      newCarDocumentType: '',
+      newCarDocumentNumber: ''
     }
   },
   methods: {
@@ -66,16 +72,23 @@ export default {
         msg: result.msg
       }
     },
+    getName (value) {
+      return value2name(value, ChinaAddressV4Data)
+    },
     booking() {
       if (this._isAllValid()) {
         const bookingReq = {
           serviceType: this.serviceType,
           bookingDate: this.bookingDate,
           bookingTime: this.bookingTime,
+          carname: this.carname,
           carId: this.carId,
-          bookingName: this.username,
-          bookingPhone: this.phone,
-          remark: this.remark,
+          engineNumber: this.engineNumber,
+          oldCarOwner: this.oldCarOwner,
+          newCarOwner: this.newCarOwner,
+          immigrationAddress: this.getName(this.immigrationAddress),
+          newCarDocumentType: this.newCarDocumentType,
+          newCarDocumentNumber: this.newCarDocumentNumber,
           createTime: new Date().getTime(),
           createBy: localStorage.getItem('username')
         }
@@ -95,8 +108,8 @@ export default {
       }
     },
     _isAllValid() {
-      if (this.username && this.phone && this.carId) {
-        if (this.$refs.username.valid && this.$refs.phone.valid && this.$refs.carId.valid) {
+      if (this.oldCarOwner && this.carname && this.carId && this.newCarOwner && this.newCarDocumentType && this.newCarDocumentNumber) {
+        if (this.$refs.oldCarOwner.valid && this.$refs.carname.valid && this.$refs.carId.valid && this.$refs.newCarOwner.valid && this.$refs.newCarDocumentNumber.valid) {
           return true
         }
         this.showToast = true
@@ -110,7 +123,6 @@ export default {
     }
   },
   created() {
-    console.log(ChinaAddressV4Data)
     if (!this.selectedDate) {
       this.$router.push('/')
     }
@@ -154,5 +166,19 @@ export default {
 
 .demo1-item-selected {
   border: 2px solid #0D70C9;
+}
+
+.x-address{
+  position: relative;
+}
+
+.x-address:before{
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 0;
+  border-top: 1px solid #D9D9D9;
+  left: 16px;
+  transform: scaleY(0.5);
 }
 </style>
