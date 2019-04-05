2f7ee859643b32eafabe57be41475427cfa8b317
RicharDada <631151149@qq.com>
2019-04-05 21:39:11 +0800

    Push 初始化Gift

--- src/router/index.js
+++ src/router/index.js
@@ -1,56 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/views/Index'
-import Home from '@/views/Home'
-import MyOrder from '@/views/MyOrder'
-import Individual from '@/views/Individual'
-import BookingGuohu from '@/views/BookingGuohu'
-import BookingQianchu from '@/views/BookingQianchu'
-import SchedulGuohu from '@/views/SchedulGuohu'
-import SchedulQianchu from '@/views/SchedulQianchu'
-import Remark from '@/views/Remark'
-/* eslint-disable */
-// const MyOrder = () => import('@/views/MyOrder')
-// const Individual = () => import('@/views/Individual')
-// const BookingGuohu = () => import('@/views/BookingGuohu')
-// const BookingQianchu = () => import('@/views/BookingQianchu')
-// const SchedulGuohu = () => import('@/views/SchedulGuohu')
-// const SchedulQianchu = () => import('@/views/SchedulQianchu')
-// const Remark = () => import('@/views/Remark')
-
-// 登录注册
-import Login from '@/views/Login'
-import Registe from '@/views/Registe'
-import CertificateConfirm from '@/views/CertificateConfirm'
-import ResetPassword from '@/views/ResetPassword'
-/* eslint-disable */
-// const Login = () => import('@/views/Login')
-// const Registe = () => import('@/views/Registe')
-// const CertificateConfirm = () => import('@/views/CertificateConfirm')
-// const ResetPassword = () => import('@/views/ResetPassword')
+import Gift from '@/views/Gift'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      component: Index,
-      children: [
-        { path: '', name: 'Home', component: Home },
-        { path: 'myOrder', name: 'MyOrder', component: MyOrder },
-        { path: 'individual', name: 'Individual', component: Individual },
-        { path: 'schedulguohua', name: 'Schedulguohu', component: SchedulGuohu },
-        { path: 'schedulqianchu', name: 'Schedulqianchu', component: SchedulQianchu },
-        { path: 'remark', name: 'Remark', component: Remark }
-      ]
-    },
-    { path: '/login', name: 'Login', component: Login },
-    { path: '/registe', name: 'Registe', component: Registe },
-    { path: '/certificateconfirm', name: 'CertificateConfirm', component: CertificateConfirm },
-    { path: '/resetpassword/:phone', name: 'ResetPassword', component: ResetPassword, props: true },
-    { path: 'bookingGuohu/:selectedDate', name: 'BookingGuohu', component: BookingGuohu, props: true },
-    { path: 'bookingQianchu/:selectedDate', name: 'BookingQianchu', component: BookingQianchu, props: true },
-    { path: '*', redirect: '/' }
+    { path: '/gift', name: 'Gift', component: Gift },
+    { path: '*', redirect: '/gift' }
   ]
 })
---
+++ src/views/Gift.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="gift">
+    <p>gift   sfsfsfsdf</p>
+    <div class="photo-list">
+      <div class="photo-list-item">
+        <div class="item-title">
+          <h3>风的女儿</h3>
+          <p>這本所敘述之故事以兩個女孩的成長為主軸,訴說著回教徒女孩在男性文化制度下</p>
+        </div>
+        <div class="item-content">
+          <img src="/static/s6394521.jpg" alt="">
+        </div>
+      </div>
+      <div class="photo-list-item">
+        <div class="item-title">
+          <h3>风的女儿</h3>
+          <p>這本所敘述之故事以兩個女孩的成長為主軸,訴說著回教徒女孩在男性文化制度下</p>
+        </div>
+        <div class="item-content">
+          <img src="/static/s6394521.jpg" alt="">
+        </div>
+      </div>
+      <div class="photo-list-item">
+        <div class="item-title">
+          <h3>风的女儿</h3>
+          <p>這本所敘述之故事以兩個女孩的成長為主軸,訴說著回教徒女孩在男性文化制度下,沒有「屈服」可言。但書中女主角─莎芭努在成長</p>
+        </div>
+        <div class="item-content">
+          <img src="/static/s6394521.jpg" alt="">
+        </div>
+      </div>
+      <div class="photo-list-item">
+        <div class="item-title">
+          <h3>风的女儿</h3>
+          <p>這本所敘述之故事以兩個女孩的成長為主軸,訴說著回教徒女孩在男性文化制度下,沒有「屈服」可言。但書中女主角─莎芭努在成長</p>
+        </div>
+        <div class="item-content">
+          <img src="/static/s6394521.jpg" alt="">
+        </div>
+      </div>
+    </div>
+
+    <div class="footer">
+      Made-By-Dadatang 2019-4-10 
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    components: {
+    },
+    data () {
+      return {
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped lang="less">
+.photo-list{
+  clear: both;
+  overflow: hidden;
+}
+.photo-list-item{
+  width: 50%;
+  float: left;
+  padding: 5px 10px;
+  box-sizing: border-box;
+  .item-title{
+    text-align: center;
+    h3{
+      font-weight: 600;
+      height: 32px;
+      line-height: 32px;;
+      font-size: 14px;
+    }
+    p{
+      font-size: 12px;
+      padding: 0 20px;
+    }
+  }
+  .item-content{
+    height: 200px;
+    margin: 0 auto;
+    border: 1px solid #ccc;
+    position: relative;
+    img{
+      position: absolute;
+      top: 10px;
+      left: 10px;
+    }
+  }
+}
+
+.footer{
+  height: 40px;
+  line-height: 40px;  
+  text-align: center;
+  font-size: 14px;
+}
+</style>
