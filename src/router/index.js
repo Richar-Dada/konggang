fab367aa15abd355b9855d13cc6d2dd45bba4589
Richar-Dada <631151149@qq.com>
2020-02-08 07:48:37 +0800

    Revert "feat：公告"
    This reverts commit e7207f081d2c9a6e51a1d941ecf18ac406b3ddfd.

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Index from '@/views/Index'
-// import Home from '@/views/Home'
-// import MyOrder from '@/views/MyOrder'
-// import Individual from '@/views/Individual'
-// import BookingGuohu from '@/views/BookingGuohu'
-// import BookingQianchu from '@/views/BookingQianchu'
-// import SchedulGuohu from '@/views/SchedulGuohu'
-// import SchedulQianchu from '@/views/SchedulQianchu'
-// import Remark from '@/views/Remark'
-import Notice from '@/views/Notice'
+import Index from '@/views/Index'
+import Home from '@/views/Home'
+import MyOrder from '@/views/MyOrder'
+import Individual from '@/views/Individual'
+import BookingGuohu from '@/views/BookingGuohu'
+import BookingQianchu from '@/views/BookingQianchu'
+import SchedulGuohu from '@/views/SchedulGuohu'
+import SchedulQianchu from '@/views/SchedulQianchu'
+import Remark from '@/views/Remark'
 /* eslint-disable */
 // const MyOrder = () => import('@/views/MyOrder')
 // const Individual = () => import('@/views/Individual')
@@ -34,25 +33,24 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   component: Index,
-    //   children: [
-    //     { path: '', name: 'Home', component: Home },
-    //     { path: 'myOrder', name: 'MyOrder', component: MyOrder },
-    //     { path: 'individual', name: 'Individual', component: Individual },
-    //     { path: 'schedulguohua', name: 'Schedulguohu', component: SchedulGuohu },
-    //     { path: 'schedulqianchu', name: 'Schedulqianchu', component: SchedulQianchu },
-    //     { path: 'remark', name: 'Remark', component: Remark }
-    //   ]
-    // },
-    // { path: '/login', name: 'Login', component: Login },
-    // { path: '/registe', name: 'Registe', component: Registe },
-    // { path: '/certificateconfirm', name: 'CertificateConfirm', component: CertificateConfirm },
-    // { path: '/resetpassword/:phone', name: 'ResetPassword', component: ResetPassword, props: true },
-    // { path: 'bookingGuohu/:selectedDate', name: 'BookingGuohu', component: BookingGuohu, props: true },
-    // { path: 'bookingQianchu/:selectedDate', name: 'BookingQianchu', component: BookingQianchu, props: true },
-    { path: '/notice', component: Notice },
-    { path: '*', redirect: '/notice' }
+    {
+      path: '/',
+      component: Index,
+      children: [
+        { path: '', name: 'Home', component: Home },
+        { path: 'myOrder', name: 'MyOrder', component: MyOrder },
+        { path: 'individual', name: 'Individual', component: Individual },
+        { path: 'schedulguohua', name: 'Schedulguohu', component: SchedulGuohu },
+        { path: 'schedulqianchu', name: 'Schedulqianchu', component: SchedulQianchu },
+        { path: 'remark', name: 'Remark', component: Remark }
+      ]
+    },
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/registe', name: 'Registe', component: Registe },
+    { path: '/certificateconfirm', name: 'CertificateConfirm', component: CertificateConfirm },
+    { path: '/resetpassword/:phone', name: 'ResetPassword', component: ResetPassword, props: true },
+    { path: 'bookingGuohu/:selectedDate', name: 'BookingGuohu', component: BookingGuohu, props: true },
+    { path: 'bookingQianchu/:selectedDate', name: 'BookingQianchu', component: BookingQianchu, props: true },
+    { path: '*', redirect: '/' }
   ]
 })
