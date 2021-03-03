25683c27388366036bccef99363cde2bdd977818
Richar-Dada <631151149@qq.com>
2021-03-03 23:38:18 +0800

    feat: 精简首页

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,6 @@ export default {
 
 <style lang="less">
 @import '~vux/src/styles/reset.less';
-@import './assets/css/common.less';
-@import './style/theme.less';
 
 body {
   background-color: #ffffff;
